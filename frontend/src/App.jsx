b1d978dba52d18c733b822134855f9eb2128d6bd
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-24 12:42:40 +0300

    Making React App and Fetching Data from DB

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}/>
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
