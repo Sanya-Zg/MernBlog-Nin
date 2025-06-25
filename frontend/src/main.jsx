74d65aea43f2561c7a8ff611d83830c1f4bde1da
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-25 08:12:36 +0300

    creating global Context and deleting functional

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { WorkoutsContextProvider } from './context/WorkoutContex.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
   </StrictMode>,
-)
+);
